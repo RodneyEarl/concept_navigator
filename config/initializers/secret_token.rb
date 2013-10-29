9c949b6a6338a5044a08b481ac4349ef4e64380c
Rodney Earl <rsearl.90@gmail.com>
2013-10-29 10:36:44 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConceptNavigator::Application.config.secret_key_base = '216a0059903184dd6d2723de1e0913eef422f63300a0db3b15cf47243dd747d9eb485f0f27dfd19ca30fb1024d2e3c027896b224f4518a0723b61e21ef7d3661'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ConceptNavigator::Application.config.session_store :cookie_store, key: '_concept_navigator_session'
